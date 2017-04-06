7d68f62a0862fbe5bc3706d8ae674d3f5c7933ab
AndrewBeers <Andrew_Beers@alumni.brown.edu>
2017-04-06 00:17:49 -0400

    Well, it's something. The network runs, but performs terribly. Run it with more power? The next step is to understand what's actually going on in there.. Maybe check out Tensorboard. Code has a lot of junk in it, TODO clean it

--- AIF_Prediction/toy_network.py
+++ AIF_Prediction/toy_network.py
@@ -1,31 +1,25 @@
-import generate_data
-import random
-import numpy as np
+'''
+A Dynamic Recurrent Neural Network (LSTM) implementation example using
+TensorFlow library. This example is using a toy dataset to classify linear
+sequences. The generated sequences have variable length.
+Long Short Term Memory paper: http://deeplearning.cs.cmu.edu/pdfs/Hochreiter97_lstm.pdf
+Author: Aymeric Damien
+Project: https://github.com/aymericdamien/TensorFlow-Examples/
+'''
+
+# from __future__ import print_function
+
 import tensorflow as tf
-import matplotlib.pyplot as plt
+import random
 
-from qtim_tools.qtim_dce.dce_util import generate_AIF, parker_model_AIF, convert_intensity_to_concentration, revert_concentration_to_intensity, estimate_concentration
-
-import matplotlib.rcsetup as rcsetup
-import matplotlib
-print(rcsetup.all_backends)
-print matplotlib.matplotlib_fname()
-
-# Generated_AIF_Intensities = np.load('Generated_AIF_Intensities.npy')
-# Groundtruth_AIF_Intensities = np.load('Groundtruth_AIF_Intensities.npy')
-
-# Generated_AIF_Intensities = np.reshape(Generated_AIF_Intensities, (Generated_AIF_Intensities[0]* Generated_AIF_Intensities[1], Generated_AIF_Intensities[2]))
-# Groundtruth_AIF_Intensities = np.reshape(Groundtruth_AIF_Intensities, (Groundtruth_AIF_Intensities[0]* Groundtruth_AIF_Intensities[1], Groundtruth_AIF_Intensities[2]))
-
-SEQ_MAX_LEN = 200
 
 # ====================
-#  TOFTS DATA GENERATOR
+#  TOY DATA GENERATOR
 # ====================
-class ToftsSequenceData(object):
+class ToySequenceData(object):
     """ Generate sequence of data with dynamic length.
     This class generate samples for training:
-    - Class 0: Tofts sequences
+    - Class 0: linear sequences (i.e. [0, 1, 2, 3,...])
     - Class 1: random sequences (i.e. [1, 3, 10, 7,...])
     NOTICE:
     We have to pad each sequence to reach 'max_seq_len' for TensorFlow
@@ -33,64 +27,36 @@ class ToftsSequenceData(object):
     dimensions). The dynamic calculation will then be perform thanks to
     'seqlen' attribute that records every actual sequence length.
     """
-    def __init__(self, n_samples=1000, max_seq_len=SEQ_MAX_LEN, min_seq_len=3, max_value=1000, ktrans_range=[0,2], ve_range=[0,.95], gaussian_noise=[0,0], T1_range=[1000,1000], TR_range=[5, 5], flip_angle_degrees_range=[30,30], relaxivity_range=[.0045, .0045], hematocrit_range=[.45,.45], sequence_length_range=[90,110], time_interval_seconds_range=[2,2], injection_start_time_seconds_range=[10,10], T1_blood_range=[1440,1440], baseline_intensity=[100,100]):
-        
+    def __init__(self, n_samples=1000, max_seq_len=20, min_seq_len=3,
+                 max_value=1000):
         self.data = []
         self.labels = []
         self.seqlen = []
-
         for i in range(n_samples):
-
             # Random sequence length
-            seq_len = np.random.random_integers(*sequence_length_range)
-            
+            len = random.randint(min_seq_len, max_seq_len)
             # Monitor sequence length for TensorFlow dynamic calculation
-            self.seqlen.append(seq_len)
-            
+            self.seqlen.append(len)
             # Add a random or linear int sequence (50% prob)
-            if random.random() < .5:    
-                
-                injection_start_time_seconds = np.random.uniform(*injection_start_time_seconds_range)
-                time_interval_seconds = np.random.uniform(*time_interval_seconds_range)
-                time_interval_minutes = time_interval_seconds/60
-                scan_time_seconds = seq_len * time_interval_seconds
-
-                while injection_start_time_seconds > .8*scan_time_seconds:
-                    injection_start_time_seconds = np.random.uniform(*injection_start_time_seconds_range)
-
-                AIF = parker_model_AIF(scan_time_seconds, injection_start_time_seconds, time_interval_seconds, timepoints=seq_len)
-
-                Concentration = np.array(estimate_concentration([np.random.uniform(*ktrans_range),np.random.uniform(*ve_range)], AIF, time_interval_minutes))
-
-                Intensity = revert_concentration_to_intensity(data_numpy=Concentration, reference_data_numpy=[], T1_tissue=np.random.uniform(*T1_range), TR=np.random.uniform(*TR_range), flip_angle_degrees=np.random.uniform(*flip_angle_degrees_range), injection_start_time_seconds=injection_start_time_seconds, relaxivity=np.random.uniform(*relaxivity_range), time_interval_seconds=time_interval_seconds, hematocrit=np.random.uniform(*hematocrit_range), T1_blood=0, T1_map = [], static_baseline=np.random.uniform(*baseline_intensity)).tolist()
-
-                s = []
-                s += [[value] for value in Intensity]
-                s += [[0.] for i in range(max_seq_len - seq_len)]
-
-                # print max_seq_len, seq_len
-                # print [[0.] for i in range(max_seq_len - seq_len)]
-                # print s
-
-                # print len(Intensity)
-
-                # s = Intensity
-
+            if random.random() < .5:
+                # Generate a linear sequence
+                rand_start = random.randint(0, max_value - len)
+                s = [[float(i)/max_value] for i in
+                     range(rand_start, rand_start + len)]
+                # Pad sequence for dimension consistency
+                s += [[0.] for i in range(max_seq_len - len)]
                 self.data.append(s)
-                self.labels.append([1., 0.])        
-
+                self.labels.append([1., 0.])
             else:
                 # Generate a random sequence
-                s = [[np.random.uniform(baseline_intensity[0]*2, baseline_intensity[0]*3)]
-                     for i in range(seq_len)]
+                s = [[float(random.randint(0, max_value))/max_value]
+                     for i in range(len)]
                 # Pad sequence for dimension consistency
-                s += [[0.] for i in range(max_seq_len - seq_len)]
-
-                # print s
-
+                s += [[0.] for i in range(max_seq_len - len)]
                 self.data.append(s)
                 self.labels.append([0., 1.])
-            # print s
+            print(s)
+        # print(s)
         self.batch_id = 0
 
     def next(self, batch_size):
@@ -98,14 +64,13 @@ class ToftsSequenceData(object):
         """
         if self.batch_id == len(self.data):
             self.batch_id = 0
-        batch_data = (self.data[self.batch_id:min(self.batch_id + batch_size, len(self.data))])
+        batch_data = (self.data[self.batch_id:min(self.batch_id +
+                                                  batch_size, len(self.data))])
         batch_labels = (self.labels[self.batch_id:min(self.batch_id +
                                                   batch_size, len(self.data))])
         batch_seqlen = (self.seqlen[self.batch_id:min(self.batch_id +
                                                   batch_size, len(self.data))])
         self.batch_id = min(self.batch_id + batch_size, len(self.data))
-
-
         return batch_data, batch_labels, batch_seqlen
 
 
@@ -120,19 +85,14 @@ batch_size = 128
 display_step = 10
 
 # Network Parameters
-seq_max_len = SEQ_MAX_LEN # Sequence max length
+seq_max_len = 20 # Sequence max length
 n_hidden = 64 # hidden layer num of features
 n_classes = 2 # linear sequence or not
 
-trainset = ToftsSequenceData(n_samples=1000, max_seq_len=seq_max_len)
-testset = ToftsSequenceData(n_samples=500, max_seq_len=seq_max_len)
+trainset = ToySequenceData(n_samples=1000, max_seq_len=seq_max_len)
+testset = ToySequenceData(n_samples=500, max_seq_len=seq_max_len)
 
-# for curve in testset.data:
-#     # print curve
-#     plt.plot(curve)
-#     plt.show()
-    # fd=gd
-# print testset.data
+print len(testset.seqlen)
 
 # tf Graph input
 x = tf.placeholder("float", [None, seq_max_len, 1])
@@ -163,7 +123,8 @@ def dynamicRNN(x, seqlen, weights, biases):
 
     # Get lstm cell output, providing 'sequence_length' will perform dynamic
     # calculation.
-    outputs, states = tf.contrib.rnn.static_rnn(lstm_cell, x, dtype=tf.float32, sequence_length=seqlen)
+    outputs, states = tf.contrib.rnn.static_rnn(lstm_cell, x, dtype=tf.float32,
+                                sequence_length=seqlen)
 
     # When performing dynamic calculation, we must retrieve the last
     # dynamically computed output, i.e., if a sequence length is 10, we need
@@ -208,7 +169,8 @@ with tf.Session() as sess:
     while step * batch_size < training_iters:
         batch_x, batch_y, batch_seqlen = trainset.next(batch_size)
         # Run optimization op (backprop)
-        sess.run(optimizer, feed_dict={x: batch_x, y: batch_y, seqlen:batch_seqlen})
+        sess.run(optimizer, feed_dict={x: batch_x, y: batch_y,
+                                       seqlen: batch_seqlen})
         if step % display_step == 0:
             # Calculate batch accuracy
             acc = sess.run(accuracy, feed_dict={x: batch_x, y: batch_y,
